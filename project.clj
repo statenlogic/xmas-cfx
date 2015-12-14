723ab41a3ccfe0d81ca49f82f95e2f9544983518
Alex Curtis <alex@alexandercurtis.net>
2015-12-14 20:33:35 +0000

    Moving things around

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [clojurefx "0.1.0-SNAPSHOT"]])
+                 [clojurefx "0.1.0-SNAPSHOT"]]
+  :main xmas-cfx.core)
--- src/xmas_cfx/core.clj
+++ src/xmas_cfx/core.clj
@@ -1,32 +1,42 @@
 (ns xmas-cfx.core
   (:require [clojurefx.factory :refer [compile]]
-            [clojurefx.clojurefx :refer [run-now]])
-  (:import [javafx.scene.control Button Label]
+            [clojurefx.clojurefx :refer [run-now]]
+            [clojurefx.protocols :refer :all])
+  (:import [javafx.scene.control Button Label TextField TextArea CheckBox ComboBox]
            [javafx.scene.layout VBox HBox]
            [javafx.stage StageBuilder]
-           [javafx.scene Scene]))
-
-
-(def graph (compile [VBox {:id "TopLevelVBox"
-                           :children [Label {:text "Hi!"}
-                                      Label {:text "I'm ClojureFX!"}
-                                      HBox {:id "HorizontalBox"
-                                            :children [Button {:text "Alright."}]}]}]))
+           [javafx.scene Scene]
+           [javafx.event Event ActionEvent EventTarget]))
 
 (def ui-root (atom nil))
 
-(def stage
+(defn make-stage
+  [graph]
   (run-now
    (doto
        (.build (StageBuilder/create))
-     (.setTitle "Motivations editor")
+     (.setTitle "Xmas Card editor")
      (.setScene (Scene. (reset! ui-root graph))))))
 
-(defn start []
+(defn start [graph]
   (do
     
-    (run-now (.show stage))))
+    (run-now (.show (make-stage graph)))))
+
+(defn make-graph
+  []
+  (let [button (compile [Button {:text "Alright."}])
+        _ (set-action! button (fn [event] (prn (source event))))
+        graph (compile [VBox {:id       "TopLevelVBox"
+                              :children [Label {:text "Hi!"}
+                                         Label {:text "I'm ClojureFX!"}
+                                         TextField {}
+                                         HBox {:id       "HorizontalBox"
+                                               :children [button]}]}])]
+    graph))
 
 (defn -main
   [& args]
-  (start))
+  (start (make-graph))
+  )
+
