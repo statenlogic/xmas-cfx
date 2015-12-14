12aca19829fea5b090715aa51f2462b285399c5d
Alex Curtis <alex@alexandercurtis.net>
2015-12-14 21:13:29 +0000

    WIP: trying to get image in stack page

--- src/xmas_cfx/core.clj
+++ src/xmas_cfx/core.clj
@@ -3,7 +3,7 @@
             [clojurefx.clojurefx :refer [run-now]]
             [clojurefx.protocols :as protocols])
   (:import [javafx.scene.control Button Label TextField TextArea CheckBox ComboBox]
-           [javafx.scene.layout VBox HBox]
+           [javafx.scene.layout VBox HBox StackPane]
            [javafx.stage StageBuilder]
            [javafx.scene Scene]
            [javafx.event Event ActionEvent EventTarget]
@@ -14,14 +14,14 @@
 (defn make-stage
   [graph]
   (run-now
-   (doto
-       (.build (StageBuilder/create))
-     (.setTitle "Xmas Card editor")
-     (.setScene (Scene. (reset! ui-root graph))))))
+    (doto
+      (.build (StageBuilder/create))
+      (.setTitle "Xmas Card editor")
+      (.setScene (Scene. (reset! ui-root graph))))))
 
 (defn start [graph]
   (do
-    
+
     (run-now (.show (make-stage graph)))))
 
 (defn make-graph
@@ -31,10 +31,16 @@
         image (Image. "card.jpg" 100.0 100.0 false false)
         image-view (ImageView. image)
         _ (protocols/set-action! button (fn [event] (prn (protocols/source event) "text is" (.getText text-field))))
+        #_(stack-pane) #_(compile [StackPane {:id       "the stack pane"
+                                              :children [image-view
+                                                         Label {:text "Message here"}]}])
+
         graph (compile [VBox {:id       "TopLevelVBox"
                               :children [Label {:text "Xmas Card Maker"}
                                          Label {:text "Choose greeting message:"}
-                                         image-view
+                                         StackPane {:id       "the stack pane"
+                                                    :children [ImageView {:image???? image}
+                                                               Label {:text "Message here"}]}
                                          text-field
                                          HBox {:id       "HorizontalBox"
                                                :children [button]}]}])]
